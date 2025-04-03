c59dcc780b8fcf84c444adb6d7b4c48a095577a0
luosmallrui <luoxiaorui115815@qq.com>
2025-04-03 12:44:44 +0800

    feature:ADD trace

--- main.py
+++ main.py
@@ -8,11 +8,27 @@ import numpy as np
 import torch as t
 import copy
 import os
+import matplotlib.pyplot as plt
+import swanlab
+import datetime
+
+current_time = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+
+# 初始化 swanlab，experiment_name 设置为当前时间
+swanlab.init(
+    key="O37YbyiqLiKdFHyc29Xtg",
+    project="DTN",
+    experiment_name=f"{current_time}",  # 使用当前时间作为 experiment_name
+)
 
 def obs_list_to_state_vector(observation):
     obs_clone = observation.clone()
     state = np.array(obs_clone)
     return state
+
+
+dic = {}
+
 
 def clear_data(reward_dir, acc_dir, loss_dir):
     if os.path.exists(reward_dir):
@@ -33,8 +49,8 @@ def clear_data(reward_dir, acc_dir, loss_dir):
     else:
         os.makedirs(os.path.dirname(loss_dir), exist_ok=True)
 
-max_delay = ENVN.max_delay
 
+max_delay = ENVN.max_delay
 
 if __name__ == "__main__":
     subtask_num = ENVN.subtasks_dag
@@ -61,8 +77,7 @@ if __name__ == "__main__":
     Gcn_hidden_dim = 128
     Gcn_output_dim = 128
 
-
-    #GCN_model = GCNModel(input_dim=Gcn_input_dim, hidden_dim=Gcn_hidden_dim, output_dim=Gcn_output_dim)
+    # GCN_model = GCNModel(input_dim=Gcn_input_dim, hidden_dim=Gcn_hidden_dim, output_dim=Gcn_output_dim)
 
     ### DRL参数设定 ###
     network_states_dim = subtask_num * (1 + 1 + edge_num + edge_num) + 1
@@ -90,7 +105,7 @@ if __name__ == "__main__":
     # 初始化 Agent 和 ReplayBuffer
     DT_place_agent = Agent(
         actor_dims=state_dim, critic_dims=state_dim, gcn_input_dim=Gcn_input_dim,
-        gcn_hidden_dim=Gcn_hidden_dim, gcn_output_dim= Gcn_output_dim, n_actions=n_actions,
+        gcn_hidden_dim=Gcn_hidden_dim, gcn_output_dim=Gcn_output_dim, n_actions=n_actions,
         agent_idx=0, n_subtasks=subtask_num, n_edges=edge_num,
         chkpt_dir='./tmp/', alpha=0.00001, beta=0.00001, fc1=256, fc2=128, fc3=64
     )
@@ -111,18 +126,17 @@ if __name__ == "__main__":
     all_loss = []
     all_score = []
     task_spread = env.tasks[0].calculate_task_spread()
-
+    task_status_map = {}
     for i in range(N_Games):
         if i > 0:
             env = copy.deepcopy(env_backup)
-        last_state = 0    #上一个时隙在更新表示为0， 1表示上个时隙就是在卸载可以跳过
-        temp_x, temp_edges, temp_network_state, temp_ready_mask, temp_distance_mask, temp_done_mask,temp_off_mask,temp_actions, temp_reward = None, None,None,None, None,None,None, None,None,
-        next_state = 0    #下一个需要记录的状态来了，0表示没来，1表示来了
+        last_state = 0  # 上一个时隙在更新表示为0， 1表示上个时隙就是在卸载可以跳过
+        temp_x, temp_edges, temp_network_state, temp_ready_mask, temp_distance_mask, temp_done_mask, temp_off_mask, temp_actions, temp_reward = None, None, None, None, None, None, None, None, None,
+        next_state = 0  # 下一个需要记录的状态来了，0表示没来，1表示来了
         score = 0
         done = False
         episode_step = 1
         print(f"\n===== Starting Episode {i + 1} =====")
-
 
         while not done:
             # 获取节点和边
@@ -132,20 +146,23 @@ if __name__ == "__main__":
 
             # 确保特征维度匹配
             assert x.shape[1] == Gcn_input_dim, f"Mismatch in GCN input: {x.shape[1]} vs {Gcn_input_dim}"
-            assert network_state.shape[0] == network_states_dim, f"Mismatch in network state: {network_state.shape[0]} vs {network_states_dim}"
+            assert network_state.shape[
+                       0] == network_states_dim, f"Mismatch in network state: {network_state.shape[0]} vs {network_states_dim}"
 
             # 选择动作
             try:
-                actions = DT_place_agent.choose_action(x, edges, network_state, total_steps, ready_mask, distance_mask, done_mask, off_mask)
-                last_5_actions = actions[:,-5:]
-                print(last_5_actions)
+                actions = DT_place_agent.choose_action(x, edges, network_state, total_steps, ready_mask, distance_mask,
+                                                       done_mask, off_mask)
+                last_5_actions = actions[:, -5:]
+                print(last_5_actions, 11)
             except Exception as e:
                 print(f"Error in choosing action: {e}")
                 break
 
             # 环境 step
             try:
-                reward, x_, edges_, new_network_state, new_ready_mask, new_distance_mask, new_done_mask, new_off_mask = env.step(actions)
+                reward, x_, edges_, new_network_state, new_ready_mask, new_distance_mask, new_done_mask, new_off_mask = env.step(
+                    actions)
             except Exception as e:
                 print(f"Error in environment step: {e}")
                 break
@@ -155,30 +172,38 @@ if __name__ == "__main__":
                 done = True
                 ave_re = (score + reward) / episode_step
                 all_task_accuracy.append(env.return_dt_accuracy())
-                all_score.append(score+reward)
+                all_score.append(score + reward)
                 all_ave_rewards.append(ave_re)
-                env.print_all_accuracy()
+                task_status = env.print_all_accuracy()
+                combined_text = ""
+                for kp in range(len(task_status)):
+                    combined_text += f"Task {kp + 1}: {str(task_status[kp])} \n"
+                combined_text += "\n"
+                swanlab.log({f"epoch {i + 1}": [swanlab.Text(combined_text, caption=f"Epoch {i + 1}")]})
                 if env.tasks[0].task_delay == 0 and not env.tasks[0].is_completed:
                     print(f"Task failed in Episode {i + 1}!")
 
-            if np.any(last_5_actions >0.5):
+            if np.any(last_5_actions > 0.5):
                 buffer.store_transition(
                     x, edges, network_state, ready_mask, distance_mask, done_mask, off_mask,
-                    actions, reward, x_, edges_, new_network_state, new_ready_mask, new_distance_mask, new_done_mask, new_off_mask, done
+                    actions, reward, x_, edges_, new_network_state, new_ready_mask, new_distance_mask, new_done_mask,
+                    new_off_mask, done
                 )
             else:
-                if last_state == 0:  #上一个时隙还在更新
-                    temp_x, temp_edges, temp_network_state, temp_ready_mask, temp_distance_mask, temp_done_mask,temp_off_mask,temp_actions, temp_reward = x, edges, network_state, ready_mask, distance_mask, done_mask, off_mask, actions, reward
+                if last_state == 0:  # 上一个时隙还在更新
+                    temp_x, temp_edges, temp_network_state, temp_ready_mask, temp_distance_mask, temp_done_mask, temp_off_mask, temp_actions, temp_reward = x, edges, network_state, ready_mask, distance_mask, done_mask, off_mask, actions, reward
                     last_state = 1
                 else:
                     if not np.array_equal(ready_mask, new_ready_mask) or done:
                         buffer.store_transition(
-                            temp_x, temp_edges, temp_network_state, temp_ready_mask, temp_distance_mask, temp_done_mask,temp_off_mask,temp_actions, temp_reward, x_, edges_, new_network_state, new_ready_mask, new_distance_mask, new_done_mask, new_off_mask, done
+                            temp_x, temp_edges, temp_network_state, temp_ready_mask, temp_distance_mask, temp_done_mask,
+                            temp_off_mask, temp_actions, temp_reward, x_, edges_, new_network_state, new_ready_mask,
+                            new_distance_mask, new_done_mask, new_off_mask, done
                         )
                         last_state = 0
 
-            #temp_x, temp_edges, temp_network_state, temp_ready_mask, temp_distance_mask, temp_done_mask,temp_off_mask,temp_actions, temp_reward = x, edges, network_state, ready_mask, distance_mask, done_mask, off_mask,
-            #actions, reward
+            # temp_x, temp_edges, temp_network_state, temp_ready_mask, temp_distance_mask, temp_done_mask,temp_off_mask,temp_actions, temp_reward = x, edges, network_state, ready_mask, distance_mask, done_mask, off_mask,
+            # actions, reward
             # if env.offload_state(last_5_actions): #当前时隙是在卸载
             #     if last_state == 0 and next_state ==0:
             #         #表明现在需要进行记录
@@ -190,7 +215,6 @@ if __name__ == "__main__":
             #         x, edges, network_state, ready_mask, distance_mask, done_mask, off_mask,
             #         actions, reward, x_, edges_, new_network_state, new_ready_mask, new_distance_mask, new_done_mask, new_off_mask, done
             #     )
-
 
             # 学习
             try:
@@ -205,6 +229,7 @@ if __name__ == "__main__":
             episode_step += 1
             total_steps += 1
             print(f"Episode {i + 1}, Step {episode_step}, Reward: {reward}, Score: {score}")
+        swanlab.log({"Reward": reward, "Score": score}, step=i + 1)
 
         # 保存模型
         if score > best_score:
